26d676100b6a3042b8d938da2ba58aecaa9a175b
Ulises Olivenza <ulises.olivenza@schibsted.com>
2020-02-05 22:38:17 +0100

    Fix test

---
+++ scripts/profile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+cd cmd/server
+go build -o numbers .
+chmod a+x numbers
+./numbers --concurrent-connections ${1-5} --port ${2-4000} --profile
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@ set -e
 cd cmd/server
 go build -o numbers .
 chmod a+x numbers
-./numbers --concurrent-connections ${1-5} --port ${2-4000} --profile
+./numbers --concurrent-connections ${1-5} --port ${2-4000}
